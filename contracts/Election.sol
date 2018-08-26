223bb9dc6da7bb1041611e6097ba06af75953de3
YCHino <a2106349@gmail.com>
2018-08-26 16:26:48 +0900

    Smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.11;
+
+contract Election {
+    //store condidate
+    //read condidate
+    string public candidate;
+    //constructor
+    constructor() public {
+        candidate = "John";
+    }
+}
